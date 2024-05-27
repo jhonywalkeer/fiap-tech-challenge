db550bac01937bafd465b7edc6d1948429a214e1
jhonywalkeer <mak.jhon@hotmail.com>
2024-05-27 19:43:42 -0300

    feat: implements a delete order via the endpoint providing an endpoint for consumption and performing the appropriate validations

--- src/adapters/driver/routes/orders.route.ts
+++ src/adapters/driver/routes/orders.route.ts
@@ -3,13 +3,16 @@ import { ExpressRouteHttp } from 'adapters/driver/http/express-route.http'
 import { CreateOrderControllerFactory } from '../factories/order/create-order.factory'
 import { FindAllOrdersControllerFactory } from '../factories/order/find-all-orders.factory'
 import { FindOrderByIdControllerFactory } from '../factories/order/find-order-by-id.factory'
+import { DeleteCategoryControllerFactory } from '../factories/category/delete-category.factory'
 
 export const OrdersRoute = Router()
 
 const { createOrderController } = CreateOrderControllerFactory()
 const { findOrderByIdController } = FindOrderByIdControllerFactory()
 const { findAllOrdersController } = FindAllOrdersControllerFactory()
+const { deleteCategoryController } = DeleteCategoryControllerFactory()
 
 OrdersRoute.post('/', ExpressRouteHttp(createOrderController))
 OrdersRoute.get('/:id', ExpressRouteHttp(findOrderByIdController))
 OrdersRoute.get('/', ExpressRouteHttp(findAllOrdersController))
+OrdersRoute.delete('/:id', ExpressRouteHttp(deleteCategoryController))
